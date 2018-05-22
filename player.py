be6df0d449b9db9924551a46992c8c810b87c9ae
Csongor Szigeti <csongor.szigeti@gmail.com>
2018-05-22 14:09:32 +0200

    Merge branch 'master' of github.com:CsongorSzigeti/poker-player-adam-feher
    # Conflicts:
    #	player.py

--- player.py
+++ player.py
@@ -6,13 +6,8 @@ class Player:
         for datas in game_state["players"]:
             number = datas["stack"] - datas["bet"]
             for data in datas["hole_cards"]:
-<<<<<<< HEAD
                 if data[0]["rank"] != data[1]["rank"]:
                     return number
-=======
-                if data[0]["rank"] == data[1]["rank"]:
-                    return 100
->>>>>>> master
                 else:
                     return 0
 
