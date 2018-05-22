41d8166f708511f4b0ba81287347a1887acfd207
Csongor Szigeti <csongor.szigeti@gmail.com>
2018-05-22 11:29:32 +0200

    asd

--- player.py
+++ player.py
@@ -3,7 +3,7 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        return 555
+        return 999
 
     def showdown(self, game_state):
         pass
