0707e5b9e89265fe5f8874f7b2abe2c600821f30
Csongor Szigeti <csongor.szigeti@gmail.com>
2018-05-22 13:30:29 +0200

    asd

--- player.py
+++ player.py
@@ -6,9 +6,9 @@ class Player:
         for datas in game_state["players"]:
             for data in datas["hole_cards"]:
                 if data[0].values() == data[1].values():
-                    return 100
+                    return 1000
                 else:
-                    return 919
+                    return 0
 
     def showdown(self, game_state):
         pass
