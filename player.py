f155b36527ee925d8869b66a9772512201e49510
Csongor Szigeti <csongor.szigeti@gmail.com>
2018-05-22 14:19:10 +0200

    asd

--- player.py
+++ player.py
@@ -3,13 +3,7 @@ class Player:
     VERSION = "Can't handle this"
 
     def betRequest(self, game_state):
-        for datas in game_state["players"]:
-            for data in datas["hole_cards"]:
-                number = data["stack"] - data["bet"]
-                if data[0]["rank"] != data[1]["rank"]:
-                    return number
-                else:
-                    return 0
+        return 939
 
     def showdown(self, game_state):
         pass
