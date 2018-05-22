4401371a3e399daad7d92f2e8a21b154d361a14a
Lukács Bence <lukacsb@gmail.com>
2018-05-22 13:46:21 +0200

    yap

--- player.py
+++ player.py
@@ -1,12 +1,12 @@
 
 class Player:
-    VERSION = "Default Python folding player"
+    VERSION = "Can't handle this"
 
     def betRequest(self, game_state):
         for datas in game_state["players"]:
             for data in datas["hole_cards"]:
                 if data[0]["rank"] == data[1]["rank"]:
-                    return 1000
+                    return 100
                 else:
                     return 0
 
