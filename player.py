8336a912ef2c1eeeb1ea5e2b36f49a408d40bba3
Csongor Szigeti <csongor.szigeti@gmail.com>
2018-05-22 11:57:00 +0200

    asd

--- player.py
+++ player.py
@@ -3,6 +3,11 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
+        for datas in game_state["players"]:
+            for data in datas["hole_cards"]:
+                if data[0].values() == data[1].values():
+                    return 100
+
         return 919
 
     def showdown(self, game_state):
