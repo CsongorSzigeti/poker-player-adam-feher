261d1fec302c2b4b8111d68e81009ff40fb6bda4
Csongor Szigeti <csongor.szigeti@gmail.com>
2018-05-22 13:50:18 +0200

    asd

--- player.py
+++ player.py
@@ -4,9 +4,10 @@ class Player:
 
     def betRequest(self, game_state):
         for datas in game_state["players"]:
+            number = datas["stack"] - datas["bet"]
             for data in datas["hole_cards"]:
-                if data[0]["rank"] == data[1]["rank"]:
-                    return 1000
+                if data[0]["rank"] != data[1]["rank"]:
+                    return number
                 else:
                     return 0
 
